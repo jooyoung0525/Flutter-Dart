25cfd57247ef853e435e3fe5957e06282f185361
jooyoung0525 <pjuy0525@naver.com>
2021-03-15 10:47:25 +0900

    Update async_await.dart

--- Dart/asynchronous/async_await.dart
+++ Dart/asynchronous/async_await.dart
@@ -1,17 +1,23 @@
 void main(){
-
-  checkVersion(); // 비동기 ------------- 1번,
+  checkVersion(); //       ------------- 1번
   print('hello'); //       ------------- 2번
 }
 
-Future checkVersion() async{ //비동기 함수 -> main 먼저 실행.
-var version = await lookUpVersion(); //  ------------------- 3번, await :  실행완료까지 다음 코드 실행 불가. 
-print(version); // -------------- 5번/ -------------- 5번
+Future checkVersion() async{           // 비동기 함수 -> main 먼저 실행.
+  var version = await lookUpVersion(); //  ------------------- 3번    await :  실행완료까지 다음 코드 실행 불가. 
+  print(version);                      //  ------------------- 5번
 }
 
 
 int lookUpVersion(){
-
-return 12; // ----------------- 4번
-
+  return 12;   // ----------------- 4번
 }
+
+
+/*
+  << 실행 결과 >>
+  
+  12
+  hello
+
+*/
