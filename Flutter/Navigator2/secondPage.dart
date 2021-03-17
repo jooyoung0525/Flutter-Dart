305164cf7d3bb00b41c82f200c7375a41e3659a5
jooyoung0525 <pjuy0525@naver.com>
2021-03-17 17:05:15 +0900

    Create secondPage.dart

---
+++ Flutter/Navigator2/secondPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('돌아가기'),
+          ),
+        ),
+      ),
+    );
+  }
+}
