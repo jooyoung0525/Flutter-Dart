ee8fa6d9fa93d0b0016615bf597bb03e4bf08ce4
jooyoung0525 <pjuy0525@naver.com>
2021-03-19 17:25:39 +0900

    Create main.dart

---
+++ Flutter/demo/main.dart
@@ -0,0 +1,13 @@
+import 'package:demoapp1/ranking.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Rankhome());
+  }
+}
